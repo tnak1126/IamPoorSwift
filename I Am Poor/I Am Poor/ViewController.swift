75ffe751276e26918b4090295e52e1b5a5c661ca
tnak1126 <64865857+tnak1126@users.noreply.github.com>
2020-09-09 08:32:10 -0700

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by home iMac on 5/5/20.
+//  Copyright © 2020 tnak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
